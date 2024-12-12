9c92d76b5398197896c8b72208eed7bdf784ac8e
Thushakaran <tthushakaran19@gmail.com>
2024-12-12 14:49:37 +0530

    first commit

---
+++ src/main/java/com/se/Fuel_Quota_Management_System/FuelQuotaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.se.Fuel_Quota_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelQuotaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelQuotaManagementSystemApplication.class, args);
+	}
+
+}
