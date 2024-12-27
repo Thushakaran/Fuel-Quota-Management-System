1e222b0d8129729022fdb0e81723969ab21e44dc
Gobi0516 <egobinath2001@gmail.com>
2024-12-27 21:30:50 +0530

    connect database

--- src/main/java/com/se/Fuel_Quota_Management_System/service/AdminService.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/service/AdminService.java
@@ -1,30 +1,43 @@
 package com.se.Fuel_Quota_Management_System.service;
 
-
-import com.se.Fuel_Quota_Management_System.model.Admin;
-import com.se.Fuel_Quota_Management_System.repository.AdminRepository;
+//import com.se.Fuel_Quota_Management_System.model.AdminLog;
+import com.se.Fuel_Quota_Management_System.model.Vehicle;
+import com.se.Fuel_Quota_Management_System.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-    public class AdminService {
-        @Autowired
-        AdminRepository adminRepository;
+public class AdminService {
 
-    public Admin addAdmin(Admin admin) {
-        return (Admin) adminRepository.save(admin);
-    }
-    public List<Admin> getAllAdmins() {
-        return adminRepository.findAll();
-    }
-    public void deleteAdmin(Long id) {
-        adminRepository.deleteById(id);
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+//    @Autowired
+//    private AdminLogRepository adminLogRepository;
+
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
     }
 
-    /*public Admin getAdminById(Long id) {
-
-    }*/
-    }
-
+////    public Vehicle updateVehicle(Long id, Vehicle updatedVehicle) {
+////        Vehicle existingVehicle = vehicleRepository.findById(id)
+////                .orElseThrow(() -> new ResourceNotFoundException("Vehicle not found"));
+////
+////        // Update vehicle details
+////        existingVehicle.setQuota(updatedVehicle.getQuota());
+////        vehicleRepository.save(existingVehicle);
+////
+////        // Log the action
+////        AdminLog log = new AdminLog("Updated Vehicle Quota", LocalDateTime.now(),admin);
+////        adminLogRepository.save(log);
+////
+////        return existingVehicle;
+////    }
+////
+////    public List<AdminLog> getAdminLogs() {
+////        return adminLogRepository.findAll();
+////    }
+}
