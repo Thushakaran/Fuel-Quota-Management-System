16e3aee4d3c042e6364308b351c2c6303c94cf2f
KeerthiGopalavannan <keerthigopalm@gmail.com>
2025-01-02 14:04:36 +0530

    create FuelTransactionService.java

--- src/main/java/com/se/Fuel_Quota_Management_System/service/FuelTransactionService.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/service/FuelTransactionService.java
@@ -1,6 +1,7 @@
 package com.se.Fuel_Quota_Management_System.service;
 
 import com.se.Fuel_Quota_Management_System.model.FuelTransaction;
+import com.se.Fuel_Quota_Management_System.model.Vehicle;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,4 +11,12 @@ public interface FuelTransactionService {
   public FuelTransaction saveTransaction(FuelTransaction fuelTransaction);
 
   public List<FuelTransaction> getAllTransaction();
+
+  public String scanVehicleQR(String qrCode);
+
+  public double fetchQuotaByVehicleId(Long vehicleId);
+
+  public FuelTransaction pumpFuel(Long vehicleId, double amount);
+
+
 }
