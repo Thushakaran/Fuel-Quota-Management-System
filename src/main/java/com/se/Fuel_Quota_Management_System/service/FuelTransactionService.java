ccd39aeefe8f5e5ccda3abe1b16572fc3ea304d0
KeerthiGopalavannan <keerthigopalm@gmail.com>
2025-01-02 14:14:17 +0530

    change in FuelTransactionService.java

--- src/main/java/com/se/Fuel_Quota_Management_System/service/FuelTransactionService.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/service/FuelTransactionService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public interface FuelTransactionService {
-  public FuelTransaction saveTransaction(FuelTransaction fuelTransaction);
+  public FuelTransaction  saveTransaction(FuelTransaction fuelTransaction);
 
   public List<FuelTransaction> getAllTransaction();
 
