cc192a9592509e70e3caba5ea6622d2c7825b461
NIROJAN1219 <karuniro19@gmail.com>
2025-01-03 00:30:05 +0530

    Add AdminRepository with method to find Admin by username

--- src/main/java/com/se/Fuel_Quota_Management_System/repository/AdminRepository.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/repository/AdminRepository.java
@@ -2,9 +2,10 @@ package com.se.Fuel_Quota_Management_System.repository;
 
 import com.se.Fuel_Quota_Management_System.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface AdminRepository
             extends JpaRepository<Admin, Long> {
 
