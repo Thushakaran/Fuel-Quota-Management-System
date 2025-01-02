091570d45a36905e379d92f1e7f123e4036abd72
NIROJAN1219 <karuniro19@gmail.com>
2025-01-03 00:16:51 +0530

    create FuelStationNotFoundException.java

---
+++ src/main/java/com/se/Fuel_Quota_Management_System/exception/FuelStationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.se.Fuel_Quota_Management_System.exception;
+
+public class FuelStationNotFoundException extends RuntimeException {
+    public FuelStationNotFoundException(String message) {
+           super(message);
+    }
+}
