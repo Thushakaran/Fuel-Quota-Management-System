2d1b39844503e59ee98ff304b1f3866e82698770
NIROJAN1219 <karuniro19@gmail.com>
2025-01-04 19:05:44 +0530

    delete get, set method and use @Data

--- src/main/java/com/se/Fuel_Quota_Management_System/model/FuelStation.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/model/FuelStation.java
@@ -23,7 +23,7 @@ public class FuelStation {
 @Column
     private String registrationNumber;
 
-    private String address;
+    private String location;
 
     // Store fuel types as a list of strings
     private List<String> fuelTypes;
@@ -36,43 +36,7 @@ public class FuelStation {
     @JoinColumn(name = "owner_id")
     private FuelStationOwner owner;
 
-    public FuelStationOwner getOwner() {
-        return owner;
-    }
 
-    public void setOwner(FuelStationOwner owner) {
-        this.owner = owner;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getStationName() {
-        return stationName;
-    }
-    public void setStationName(String stationName) {
-        this.stationName = stationName;
-    }
 
 // Repeat for other fields
 
