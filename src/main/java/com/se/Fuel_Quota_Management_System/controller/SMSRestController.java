189c3b5a17d6a643c777e6568140b0733a5790b8
Tharshana11 <tharsh.sarath@gmail.com>
2025-01-07 00:16:51 +0530

    Add code in SMSRestController

--- src/main/java/com/se/Fuel_Quota_Management_System/controller/SMSRestController.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/controller/SMSRestController.java
@@ -1,5 +1,25 @@
 package com.se.Fuel_Quota_Management_System.controller;
 
-public class SMSRestController {
+import com.se.Fuel_Quota_Management_System.service.TwilioOTPService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+public class SMSRestController {
+    @RestController
+    @RequestMapping("api/v1/")
+    public class SMSSendRequest {
+        @Autowired
+        TwilioOTPService twilioOTPService;
+        @PostMapping("/processSMS")
+        public String processSMS(@RequestBody SMSSendRequest smsSendRequest)
+        {
+            log.info("processSMS started smsRequest"+ smsSendRequest.toString());
+            return
+        }
+    }
 }
