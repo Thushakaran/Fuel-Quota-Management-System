b2d657b6d90d6175e87f4f314194dd8455070e79
Tharshana11 <tharsh.sarath@gmail.com>
2025-01-07 21:18:41 +0530

    Edit name as VehicleRegistrationNotificationController

--- src/main/java/com/se/Fuel_Quota_Management_System/controller/VehicleRegistrationNotificationController.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/controller/VehicleRegistrationNotificationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/")
-public class SMSRestController {
+public class VehicleRegistrationNotificationController {
 
         @Autowired
         TwilioOTPService twilioOTPService;
