e8e66de39b02843b27c1ecb83fee888635a836ee
Tharshana11 <tharsh.sarath@gmail.com>
2025-01-07 09:07:19 +0530

    Edit code in TwilioOTPService

--- src/main/java/com/se/Fuel_Quota_Management_System/service/TwilioOTPService.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/service/TwilioOTPService.java
@@ -2,6 +2,7 @@ package com.se.Fuel_Quota_Management_System.service;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,20 +10,21 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class TwilioOTPService {
-@Value("${twilio.account_sid}")
-String account_sid;
-@Value("${twilio.auth_token}")
-String auth_token;
-@Value("${twilio.trial_number")
-String trial_number;
 
+    @Value("${twilio.account_sid}")
+    String account_sid;
+    @Value("${twilio.auth_token}")
+    String auth_token;
+    @Value("${twilio.trial_number")
+    String trial_number;
 
-private void setup()
-{
-  Twilio.init(account_sid,auth_token);
-}
-public String sendSMS(String smsNumber,String smsMessage)
-{
-Message message=Message.creator(new PhoneNumber(smsNumber),new PhoneNumber(trial_number),smsMessage).create();
-    return smsMessage.getChars().toString();
+    @PostConstruct
+    private void setup() {
+        Twilio.init(account_sid, auth_token);
+    }
+
+    public String sendSMS(String smsNumber, String smsMessage) {
+        Message message = Message.creator(new PhoneNumber(smsNumber), new PhoneNumber(trial_number), smsMessage).create();
+        return smsMessage.getChars().toString();
+    }
 }
