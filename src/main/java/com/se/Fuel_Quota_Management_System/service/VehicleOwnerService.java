e270e0431456601b5acfdc17c53d6377a810878c
Thushakaran <tthushakaran19@gmail.com>
2025-01-11 01:08:23 +0530

    create VehicleOwnerService.java

---
+++ src/main/java/com/se/Fuel_Quota_Management_System/service/VehicleOwnerService.java
@@ -0,0 +1,7 @@
+package com.se.Fuel_Quota_Management_System.service;
+
+import com.se.Fuel_Quota_Management_System.model.VehicleOwner;
+
+public interface VehicleOwnerService {
+    VehicleOwner getOwnerById(Long id);
+}
