e0c6c9cde930e36135944dc03eef4a4e28b69503
vipusrihar <vipusrihar@gmail.com>
2025-01-10 18:32:53 +0530

    StationNavbar Created

--- frontend/src/pages/StationHomePage.jsx
+++ frontend/src/pages/StationHomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import StationNavbar from '../components/StationNavbar';
 import Footer from '../components/Footer';
 
 
@@ -9,10 +9,16 @@ const StationHomePage = () => {
     const {id} = useParams(); 
   return (
     <>
-      {/* temporay Navbar */}
-      <Navbar />    
-      <div>StationHomePage {id}</div>
-      {/* temporayFooter  */}
+      <StationNavbar />    
+      <header className="text-black text-left py-1 ps-4 ms-4">
+        <h1 className="fw-bold">{id}</h1>
+      </header>
+      <main className="container my-2 ">
+        <h2 className="fw-bold mb-3">Balance Fuels</h2>
+        <div className="row g-4">
+
+        </div>
+      </main>
       <Footer/>
     </>
     )
