4949ba1a2661f04c99f7f8d73e4a35ddc9a36600
vipusrihar <vipusrihar@gmail.com>
2025-01-16 17:01:17 +0530

    exception handling

---
+++ src/main/java/com/se/Fuel_Quota_Management_System/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.se.Fuel_Quota_Management_System.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
