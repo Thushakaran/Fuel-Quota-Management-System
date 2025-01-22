43d5069ad4533520e9f38a9f25c08ef2015b6bbe
vipusrihar <vipusrihar@gmail.com>
2025-01-22 22:11:14 +0530

    error fixed

--- src/main/java/com/se/Fuel_Quota_Management_System/repository/UserLogRepository.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/repository/UserLogRepository.java
@@ -2,9 +2,11 @@ package com.se.Fuel_Quota_Management_System.repository;
 
 import com.se.Fuel_Quota_Management_System.model.UserLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserLogRepository extends JpaRepository<UserLog,Long> {
     Optional<UserLog> findByUserName(String username);
 
