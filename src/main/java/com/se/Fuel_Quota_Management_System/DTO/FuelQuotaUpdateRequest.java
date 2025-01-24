6aa63ce7ff450e59aa816b3076a13c546d5f4530
KeerthiGopalavannan <keerthigopalm@gmail.com>
2025-01-24 16:45:36 +0530

    add FuelQuotaUpdateRequest.java

--- src/main/java/com/se/Fuel_Quota_Management_System/DTO/FuelQuotaUpdateRequest.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/DTO/FuelQuotaUpdateRequest.java
@@ -1,2 +1,16 @@
-package com.se.Fuel_Quota_Management_System.DTO;public class FuelQuotaUpdateRequest {
+package com.se.Fuel_Quota_Management_System.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FuelQuotaUpdateRequest {
+    private Long stationId;
+    private Long vehicleId;
+    private double amount;
 }
