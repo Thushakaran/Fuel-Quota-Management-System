31280fd7e0dbeaf25b7f5c4cb38c4620030eae0b
NIROJAN1219 <karuniro19@gmail.com>
2025-01-26 16:30:53 +0530

    update axioslnstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8082/api", // Replace with your backend API base URL
+  baseURL: "http://localhost:8080/api", // Replace with your backend API base URL
   headers: {
     "Content-Type": "application/json",
   },
