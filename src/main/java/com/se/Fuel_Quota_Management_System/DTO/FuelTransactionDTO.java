ce87cb944bc6b1ced78d8f107a06ee14e960fff9
KeerthiGopalavannan <keerthigopalm@gmail.com>
2025-01-26 16:24:57 +0530

    create GetFuel Transaction details

--- src/main/java/com/se/Fuel_Quota_Management_System/DTO/FuelTransactionDTO.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/DTO/FuelTransactionDTO.java
@@ -1,2 +1,22 @@
-package com.se.Fuel_Quota_Management_System.DTO;public class FuelTransactionDTO {
+package com.se.Fuel_Quota_Management_System.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FuelTransactionDTO {
+    private Long id;
+    private Long vehicleId;
+    private Double amount;
+    private LocalDateTime transactionDate;
+    private Long stationId;
+
+
 }
--- src/main/java/com/se/Fuel_Quota_Management_System/service/FuelTransactionService.java
+++ src/main/java/com/se/Fuel_Quota_Management_System/service/FuelTransactionService.java
@@ -1,5 +1,6 @@
 package com.se.Fuel_Quota_Management_System.service;
 
+import com.se.Fuel_Quota_Management_System.DTO.FuelTransactionDTO;
 import com.se.Fuel_Quota_Management_System.model.FuelTransaction;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,15 @@ public interface FuelTransactionService {
 
   public FuelTransaction startTransaction(Long vehicleId, double amount, Long stationId);
 
-  public List<FuelTransaction> getTransactionsByVehicleId(Long vehicleId);
+public List<FuelTransactionDTO> getTransactionsByVehicleId(Long vehicleId);
+
+public void DeductFuelQuotaWhenPumpFuel(Long stationId, double amount, Long vehicleId);
+
+
+
+
 
 //  public void updateFuelInventory(Long stationId, double amount, Long vehicleId);
-
-
 //  public void updateVehicleFuelQuota(Long vehicleId,double amount);
 
 
