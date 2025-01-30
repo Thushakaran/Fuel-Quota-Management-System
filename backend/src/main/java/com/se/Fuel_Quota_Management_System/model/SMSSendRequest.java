15bfca7751c254a6374334df469926ffcfc21dd9
Tharshana11 <tharsh.sarath@gmail.com>
2025-01-30 11:28:20 +0530

    Editted code in SMSSendRequest

--- backend/src/main/java/com/se/Fuel_Quota_Management_System/model/SMSSendRequest.java
+++ backend/src/main/java/com/se/Fuel_Quota_Management_System/model/SMSSendRequest.java
@@ -1,7 +1,8 @@
 package com.se.Fuel_Quota_Management_System.model;
 
 public class SMSSendRequest {
-    private String phoneNumber;
+    public static String phoneNumber;
+
 
     // Getters and Setters
     public String getPhoneNumber() {
