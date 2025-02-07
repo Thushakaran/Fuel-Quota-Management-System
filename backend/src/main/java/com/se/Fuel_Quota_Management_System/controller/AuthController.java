d67f33fca4ddd0557bc8156c6bcd59a33d0abac0
Gobi0516 <egobinath2001@gmail.com>
2025-02-07 11:51:48 +0530

    Add login for mobileApp

--- backend/src/main/java/com/se/Fuel_Quota_Management_System/controller/AuthController.java
+++ backend/src/main/java/com/se/Fuel_Quota_Management_System/controller/AuthController.java
@@ -1,12 +1,17 @@
 package com.se.Fuel_Quota_Management_System.controller;
 
 import com.se.Fuel_Quota_Management_System.DTO.AuthResponse;
+import com.se.Fuel_Quota_Management_System.model.FuelStation;
+import com.se.Fuel_Quota_Management_System.model.FuelStationOwner;
+import com.se.Fuel_Quota_Management_System.repository.FuelStationOwnerRepository;
+import com.se.Fuel_Quota_Management_System.repository.FuelStationRepository;
 import com.se.Fuel_Quota_Management_System.security.JwtUtil;
 import com.se.Fuel_Quota_Management_System.DTO.RegisterRequest;
 import com.se.Fuel_Quota_Management_System.model.Role;
 import com.se.Fuel_Quota_Management_System.model.UserLog;
 import com.se.Fuel_Quota_Management_System.repository.RoleRepository;
 import com.se.Fuel_Quota_Management_System.repository.UserLogRepository;
+import com.se.Fuel_Quota_Management_System.service.AdminService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,11 +33,12 @@ import java.util.Optional;
 @AllArgsConstructor
 public class AuthController {
 
-    @Autowired
-    private  AuthenticationManager authenticationManager;
 
     @Autowired
-    private  JwtUtil jwtUtil;
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Autowired
     private UserLogRepository userLogRepository;
@@ -43,6 +49,11 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private FuelStationRepository fuelStationRepository;
+
+    @Autowired
+    private FuelStationOwnerRepository fuelStationOwnerRepository;
 
 
     //register user
@@ -83,11 +94,16 @@ public class AuthController {
             UserLog user = userLogRepository.findByUserName(loginRequest.getUserName())
                     .orElseThrow(() -> new RuntimeException("User not found"));
 
+            FuelStationOwner fuelStationOwner = fuelStationOwnerRepository.findFuelStationOwnerByOwnerLogId(user.getId());
             // Generate JWT token
             String token = jwtUtil.generateToken(user.getUserName());
 
+            FuelStation fuelStation = fuelStationRepository.findByOwnerId(fuelStationOwner.getId())
+                    .orElseThrow(() -> new RuntimeException("Fuel station not found with owner id: " + fuelStationOwner.getId()));
+
             // Create response
-            AuthResponse authResponse = new AuthResponse(token, user.getRole(),user.getId());
+            AuthResponse authResponse = new AuthResponse(token, user.getRole(), user.getId(), fuelStation.getId());
+
             //generate JWT token with roles
 
             return ResponseEntity.ok(authResponse);
